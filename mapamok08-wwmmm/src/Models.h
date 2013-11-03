53cc5b8f343e7ee19e58aaec08ac66e0a6beaa55
Saqoosha <a@saqoo.sh>
2013-11-03 13:01:17 +0900

    Ball ついか

--- mapamok08-wwmmm/src/Models.h
+++ mapamok08-wwmmm/src/Models.h
@@ -8,6 +8,8 @@
 
 #pragma once
 
+#include "ofxAssimpModelLoader.h"
+
 
 class Stage : public ofNode {
 public:
@@ -40,3 +42,27 @@ private:
     ofVec3f euler_angles_;
 };
 
+
+
+class Ball : public ofNode {
+public:
+    void setup() {
+        loader_.loadModel("ball.obj");
+        ofPoint sceneMax = loader_.getSceneMax();
+        ofPoint sceneMin = loader_.getSceneMin();
+        float size = MAX(sceneMax.x - sceneMin.x, MAX(sceneMax.y - sceneMin.y, sceneMax.z - sceneMin.z));
+        setScale(5.4 * 2 * 0.3 / size);
+        ball_ = loader_.getMesh(0);
+    }
+    
+    void customDraw() {
+        ofPushStyle();
+        ofSetLineWidth(1);
+        ofSetColor(255, 0, 0, 128);
+        ball_.drawWireframe();
+        ofPopStyle();
+    }
+    
+    ofxAssimpModelLoader loader_;
+    ofMesh ball_;
+};
